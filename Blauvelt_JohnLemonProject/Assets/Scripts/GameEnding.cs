708f690ccf8415e30b792103568aaf705920aed6
Aubrie Blauvelt <aublauve@iu.edu>
2020-10-10 16:40:57 -0400

    Completed Part 7
    Completed Part 7 and edited script for Observer and Game Ending

--- Blauvelt_JohnLemonProject/Assets/Scripts/GameEnding.cs
+++ Blauvelt_JohnLemonProject/Assets/Scripts/GameEnding.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f; 
     public GameObject player;
-    public CanvasGroup exitBackgroundImageCanvasGroup; 
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
     bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
 
     float m_Timer; 
 
@@ -22,23 +25,41 @@ public class GameEnding : MonoBehaviour
   
     }
 
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
     void Update()
     {
         if(m_IsPlayerAtExit)
         {
-            Endlevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if(m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
         }
     }
 
-    void Endlevel()
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
+
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
         exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
 
         if(m_Timer > fadeDuration +displayImageDuration)
         {
-            Application.Quit();
+           if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
