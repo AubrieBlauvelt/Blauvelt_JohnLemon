73c92145c47426b38bd97e52e6e2caac2d372862
Aubrie Blauvelt <aublauve@iu.edu>
2020-10-20 21:24:04 -0400

    Created Bonk Zone Script

---
+++ Blauvelt_JohnLemonProject/Assets/Scripts/PlayerBonk.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBonk : MonoBehaviour
+{
+
+    public GameObject enemyToBonk; //saves reference to stun ability
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && enemyToBonk != null)
+        {
+            Destroy(enemyToBonk);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ghost"))
+        {
+            enemyToBonk = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ghost"))
+        {
+            if (enemyToBonk == other.gameObject)
+            {
+                enemyToBonk = null;
+            }
+        }
+    }
+}
